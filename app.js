8330157088bb1007a2f387eb42c03a6a236145ac
bilalajmal7557-glitch <bilalajmal7557@gmail.com>
2026-05-26 06:24:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 require("dotenv").config();
 const axios = require("axios");
 
-async function getToken() {
+async function createAccount(){
 
-try {
+try{
 
 const response = await axios.post(
-"https://business-api.tiktok.com/open_api/v1.3/oauth2/access_token/",
+"https://business-api.tiktok.com/open_api/v1.3/bc/advertiser/create/",
 {
-app_id: process.env.TIKTOK_APP_ID,
-secret: process.env.TIKTOK_SECRET,
-auth_code: process.env.TIKTOK_AUTH_CODE
+bc_id:process.env.BC_ID,
+company:"sufi agency 09",
+currency:"USD",
+timezone:"Asia/Karachi"
+},
+{
+headers:{
+"Access-Token":process.env.TIKTOK_TOKEN,
+"Content-Type":"application/json"
+}
 }
 );
 
 console.log(response.data);
 
-} catch(error){
+}catch(error){
 
 console.log(
 error.response?.data || error.message
@@ -26,4 +33,4 @@ error.response?.data || error.message
 
 }
 
-getToken();
+createAccount();

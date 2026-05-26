390840eb437e160410aa55377be72c7851f59c48
bilalajmal7557-glitch <bilalajmal7557@gmail.com>
2026-05-26 05:56:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("TikTok API Ready");
+require("dotenv").config();
+const axios = require("axios");
+
+async function getAdvertisers() {
+ try {
+
+ const response = await axios.get(
+ "https://business-api.tiktok.com/open_api/v1.3/oauth2/advertiser/get/",
+ {
+ headers:{
+ "Access-Token":process.env.TIKTOK_TOKEN
+ }
+ }
+ );
+
+ console.log(response.data);
+
+ } catch(error){
+
+ console.log(
+ error.response?.data || error.message
+ );
+
+ }
+}
+
+getAdvertisers();

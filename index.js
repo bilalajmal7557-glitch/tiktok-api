71b006295de8db6c5b28a76bd59a37265f3f4629
bilalajmal7557-glitch <bilalajmal7557@gmail.com>
2026-05-30 14:54:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-```javascript
 require("dotenv").config();
+
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "running"
-  });
-});
-
-app.get("/create-advertiser", async (req, res) => {
-  try {
-    const response = await axios.post(
-      "https://business-api.tiktok.com/open_api/v1.3/bc/advertiser/create/",
-      {
-        bc_id: process.env.BC_ID,
-        company: "Test Marketing Agency"
-      },
-      {
-        headers: {
-          "Access-Token": process.env.TIKTOK_ACCESS_TOKEN,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json(
-      error.response?.data || {
-        error: error.message
-      }
-    );
-  }
+  res.json({ status: "running" });
 });
 
 const PORT = process.env.PORT || 3000;
@@ -42,4 +14,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-```

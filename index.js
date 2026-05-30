68a0747f7d955e2cf96a1c34f69f651b7b8d44e7
bilalajmal7557-glitch <bilalajmal7557@gmail.com>
2026-05-30 10:23:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-
-const app = express();
-
-app.get("/", async (req, res) => {
-  try {
-    const response = await axios.post(
-      "https://business-api.tiktok.com/open_api/v1.3/oauth2/access_token/",
-      {
-        app_id: process.env.APP_ID,
-        secret: process.env.APP_SECRET,
-        auth_code: req.query.code
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.json(error.response?.data || error.message);
+const response = await axios.post(
+  "https://business-api.tiktok.com/open_api/v1.3/oauth2/access_token/",
+  {
+    app_id: process.env.TIKTOK_APP_ID,
+    secret: process.env.TIKTOK_SECRET,
+    auth_code: req.query.code
   }
-});
-
-app.listen(process.env.PORT || 3000);
+);
